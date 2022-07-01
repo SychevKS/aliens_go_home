87584076821ee263db714fa1bf5715938c7fdfdf
Sychov Kirill <ksychov@pminst.ru>
2022-07-01 16:43:56 +0500

    Исправление счета жизни

--- src/redux/reducers/moveObjects.js
+++ src/redux/reducers/moveObjects.js
@@ -6,21 +6,26 @@ import { calculateAngle } from "../../utils/formulas";
 export default (state, action) => {
     const gameState = createFlyingObjects(state.gameState);
     const objectsDestroyed = checkCollisions(state.gameState.cannonBalls, state.gameState.flyingObjects);
+   
+    let flyingObjects = gameState.flyingObjects.filter(item => (new Date().getTime() - item.createdAt) < 4000)
+    
+    const lives = state.gameState.flyingObjects.length > flyingObjects.length ? state.gameState.lives - 1 : state.gameState.lives;
+
     const cannonBallsDestroyed = objectsDestroyed.map(object => (object.cannonBallId));
     const flyingDiscsDestroyed = objectsDestroyed.map(object => (object.flyingDiscId));
-     
-    const flyingObjects = gameState.flyingObjects
-        .filter(item => (new Date().getTime() - item.createdAt) < 4000)
-        .filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)))
 
     const cannonBalls = moveBalls(state.gameState.cannonBalls).filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)))
+    
+    flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)))
 
-    const lives = state.gameState.flyingObjects.length > flyingObjects.length ? state.gameState.lives - 1 : state.gameState.lives;
+    const start = state.gameState.lives != 3 && lives <= 0; 
+    console.log(state.gameState.lives != 3)
 
     return {
         angle: calculateAngle(0, 0, action.payload.x, action.payload.y),
         gameState: {
             ...gameState,
+            started: start,
             lives: lives > 0 ? lives : 3,
             flyingObjects: lives > 0 ? flyingObjects : [],
             cannonBalls:  lives > 0 ? cannonBalls : [],
